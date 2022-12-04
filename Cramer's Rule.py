0a1052cc4cdc16b217b5bde99f65790fe848298d
Syn <92839166+LydiUX@users.noreply.github.com>
2022-12-04 11:09:21 -0800

    ff

--- Cramer's Rule.py
+++ Cramer's Rule.py
@@ -1,7 +1,7 @@
 def systemSolution(coefficientMatrix, solutionVector, whichSolution):
     
     #Duplicates the coefficientMatrix as any matrix with matrix1 = matrix2 will share the same memory
-    startingCoefMatrix = [[0]*len(coefficientMatrix) for i in range(len(coefficientMatrix))]
+    startingCoefMatrix = [[0]*len(coefficientMatrix) for _ in range(len(coefficientMatrix))]
     for i in range(len(coefficientMatrix)):
         for j in range(len(coefficientMatrix)):
             startingCoefMatrix[i][j] = coefficientMatrix[i][j]
@@ -33,26 +33,38 @@ def systemSolution(coefficientMatrix, solutionVector, whichSolution):
                         determinant *= matrix[i][j]
             return determinant
         except:
-            return Exception(f'{matrix} must be square.')
+            raise Exception(f'{matrix} must be square.')
     
     def newSolutionMatrix(matrix, solution, index):
-        lenSolution = len(solution)
-        lenCoef = len(matrix)
-        for i in range(lenCoef):
-            for x in range(lenSolution):
+        for _ in range(len(matrix)):
+            for x in range(len(solution)):
                 matrix[x][index] = solution[x]
         return matrix
 
     if whichSolution >= 1:
         try:
-            return f'Solution x{whichSolution}: {det(newSolutionMatrix(coefficientMatrix, solutionVector, whichSolution - 1)) / det(startingCoefMatrix)}'
+            return f'Solution x_{whichSolution}: {det(newSolutionMatrix(coefficientMatrix, solutionVector, whichSolution - 1)) / det(startingCoefMatrix)}'
         except ZeroDivisionError:
             return f'This system may have infinitely many solutions, or no solutions.'
         except:
-            return f'Solution x{whichSolution} is out of range!'
+            return f'Solution x_{whichSolution} is out of range!'
     else:
-        return f'Solution x{whichSolution} is out of range!'
-    
-A = [[1,2,3],[3,4,6],[5,6,7]]
-B = [5,6,7]
-print(systemSolution(A, B, 3))
+        return f'Solution x_{whichSolution} is out of range!'
+
+#User interaction
+try:
+    print('This calculator solves linear systems of equations in the form of\na_0x_1 + a_1x_2 + ... + a_mx_n = s_1\nb_0x_1 + b_1x_2 + ... + b_mx_n = s_2\netc...')
+    size = int(input('How many variables does your system have? '))
+    print('Enter the coefficients of your equations:')
+    coefMatrix = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(len(coefMatrix)):
+        for j in range(len(coefMatrix)):
+            coefMatrix[i][j] = float(input(f'Coefficient of equation {i+1} variable {j+1}: '))
+    print(f'Your system of equations should have {size} solutions.\nEnter the solutions of your equations:')
+    solution = [1 for _ in range(size)]
+    for i in range(size):
+        solution[i] = float(input(f'Solution corresponding to equation {i + 1}: '))
+    sol = int(input('Which solution would you like to see? '))
+    print(systemSolution(coefMatrix, solution, sol))
+except:
+    print('Please provide number answers to the questions.')
