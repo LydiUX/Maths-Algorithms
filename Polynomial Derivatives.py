92d3717e3142c30bedc454790d814c3b24e04afe
Jack Shi <shijacklol@gmail.com>
2023-06-21 08:34:54 -0700

    Create Polynomial Derivatives.py

---
+++ Polynomial Derivatives.py
@@ -0,0 +1,31 @@
+#polynomial derivatives
+
+def polynomialDerivative(format, *args):
+    coefList = []
+    for coefs in args:
+        coefList.append(coefs)
+    for i in range(len(coefList)):
+        coefList[i] *= len(coefList) - i - 1
+    coefList = coefList[-1:] + coefList[:-1]
+    coefList.pop(0)
+    if format == 'l':
+        return coefList
+    elif format == 'f':
+        def polynomial(x):
+            sum = 0
+            for i in range(len(coefList)):
+                sum += coefList[i] * x ** (len(coefList) - i - 1)
+            return sum
+        return polynomial
+    elif format == 'p':
+        coefListStr = [str(i) for i in coefList]
+        for i in range(len(coefList)):
+            if (len(coefListStr) - i - 1) != 0:
+                coefListStr[i] = coefListStr[i] + "*x^" + str((len(coefListStr) - i - 1)) + "+"
+        return 'P\'(x)=' + ''.join(coefListStr)
+    
+def evaluatePolynomial(func, evalpoint):
+    return func(evalpoint)
+
+print(polynomialDerivative('printed', 9,4,3,2))
+print(evaluatePolynomial(polynomialDerivative('function', 9,4,3,2), 4))
