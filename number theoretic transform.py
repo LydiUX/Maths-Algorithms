b5abc638efef931cfd09773b6913ac8b08e49a36
Jack Shi <shijacklol@gmail.com>
2023-07-15 14:25:34 -0700

    Update number theoretic transform.py

--- number theoretic transform.py
+++ number theoretic transform.py
@@ -1,44 +1,72 @@
 #number theoretic transform
+import math
 
-def NTT(polynomial, generator, ring):
-    if len(polynomial) == 1: #number theoretic transform under galois field GF(ring)
-        return polynomial #base case, can't NTT if polynomial has length 1
-    polynomial_even = polynomial[::2] #divide and conquer!
+def NTT(polynomial, size, generator, modulus): 
+    '''
+    Size refers to the size of the polyonmial. The size of the polynomial is a power of 2; i.e. 2^k. Constraint: (size | modulus - 1).
+    The maximum degree of the polynomial is n-1. 
+    The generator is the primitive root of unity under the galois field F_modulus.
+    '''
+    if size == 1:
+        return polynomial
+    polynomial_even = polynomial[::2] #we use a divide and conquer approach
     polynomial_odd = polynomial[1::2]
-    eval_point = generator ** 2 % ring #evaluate each even/odd function at 2^(2k)
-    NTT(polynomial_even, eval_point, ring)
-    NTT(polynomial_odd, eval_point, ring)
-    eval_point = 1
-    for i in range(len(polynomial)):
-        polynomial[i] = (polynomial_even[i % (len(polynomial) // 2)] + eval_point * polynomial_odd[i % (len(polynomial) // 2)]) % ring
-        eval_point = (eval_point * generator) % ring
+    eval_point = generator**2 % modulus
+    polynomial_even = NTT(polynomial_even, size // 2, eval_point, modulus)
+    polynomial_odd = NTT(polynomial_odd, size // 2, eval_point, modulus)
+    for i in range(size // 2):
+        polynomial[i] = (polynomial_even[i] + generator**i * polynomial_odd[i]) % modulus
+        polynomial[i + size // 2] = (polynomial_even[i] + generator**(i + size // 2) * polynomial_odd[i]) % modulus
     return polynomial
 
 def extended_gcd(a, b):
     if b == 0:
         return a, 1, 0
-    else:
-        gcd, x, y = extended_gcd(b, a % b)
-        return gcd, y, x - (a // b) * y
+    gcd, x, y = extended_gcd(b, a % b)
+    return gcd, y, x - (a // b) * y
     
-def modular_inverse(a, m):
-    gcd, x, _ = extended_gcd(a, m)
+def modular_inverse(a, modulus):
+    gcd, x, _ = extended_gcd(a, modulus)
     if gcd != 1:
-        raise ValueError
-    else:
-        return x % m
+        raise ValueError #modular multiplicative inverse does not exist
+    return x % modulus
 
-def INTT(polynomial, generator, ring):
-    if len(polynomial) == 1:
+def INTT(polynomial, size, generator, modulus):
+    if size == 1:
         return polynomial
-    for i in range(1, (len(polynomial) + 1) // 2):
-        temp = polynomial[i]
-        polynomial[i] = polynomial[len(polynomial) - i]
-        polynomial[len(polynomial) - i] = temp
-    NTT(polynomial, generator, ring)
-    for i in range(len(polynomial)):
-        polynomial[i] = (modular_inverse(len(polynomial), ring) * polynomial[i]) % ring
+    for i in range(1, (size + 1) // 2):
+        polynomial[size - i], polynomial[i] = polynomial[i], polynomial[size - i]
+    NTT(polynomial, size, generator, modulus)
+    for i in range(size):
+        polynomial[i] = (modular_inverse(size, modulus) * polynomial[i]) % modulus
     return polynomial
 
-print(NTT([1,4,4,3], 2, 5))
-print(INTT([2,4,3,0], 2, 5))
+def modular_power(a, b, modulus):
+    if a == 0:
+        return 0 
+    result = 1
+    a %= modulus
+    while b > 0:
+        if b & 1 == 1:
+            result *= a % modulus
+        b >>= 1
+        a *= a % modulus
+    return result
+
+def find_galois_field(k):
+    '''
+    we want to find a suitable field such that generator^(2^(k-1)) = -1 mod modulus
+    and N = 2^(ak) + 1
+    '''
+    a = 1
+    while 1:
+        modulus = (a << k) + 1 #equivalent to 2^(ak) + 1
+        for generator in range(2, a + 1):
+            if modular_power(generator, 1 << (k - 1), modulus) == modulus - 1:
+                return modulus, generator
+        a += 1
+    
+
+assert(NTT([1,4,4,3], 4, 2, 5)) == [2,4,3,0]
+assert(INTT([2,4,3,0], 4, 2, 5)) == [1,4,4,3]
+print(find_galois_field(4))
